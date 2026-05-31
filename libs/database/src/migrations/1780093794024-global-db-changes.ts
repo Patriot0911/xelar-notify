cbe2a6998cdc96dc76a1988ecd53fa3635875425
Patriot <alexandr.potaskalov@gmail.com>
2026-06-01 02:56:55 +0300

    fix: fix migrations

--- libs/database/src/migrations/1780093794024-global-db-changes.ts
+++ libs/database/src/migrations/1780093794024-global-db-changes.ts
@@ -4,30 +4,22 @@ export class GlobalDbChanges1780093794024 implements MigrationInterface {
     name = 'GlobalDbChanges1780093794024'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "user_sessions" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "user_id" uuid NOT NULL, "refresh_token_hash" character varying NOT NULL, "expires_at" TIMESTAMP WITH TIME ZONE NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_e93e031a5fed190d4789b6bfd83" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "discord_guilds" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "balance" numeric(3,1) NOT NULL DEFAULT '0', "created_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_0549d637a8cf188fc8290f4313b" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TYPE "public"."discord_notifications_status_enum" AS ENUM('active', 'suspended')`);
         await queryRunner.query(`CREATE TYPE "public"."discord_notifications_cost_type_enum" AS ENUM('personal', 'credit', 'guild')`);
         await queryRunner.query(`CREATE TABLE "discord_notifications" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "owner_id" uuid NOT NULL, "cost" numeric(3,1) NOT NULL, "status" "public"."discord_notifications_status_enum" NOT NULL DEFAULT 'active', "cost_type" "public"."discord_notifications_cost_type_enum" NOT NULL, "channel_id" character varying NOT NULL, "guild_id" uuid NOT NULL, "streamer_event_id" uuid NOT NULL, "message_payload" jsonb, "created_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_ea7621898107c04d155c86088f1" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TYPE "public"."webhook_notifications_type_enum" AS ENUM('discord')`);
         await queryRunner.query(`CREATE TABLE "webhook_notifications" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "streamer_event_id" uuid NOT NULL, "owner_id" uuid NOT NULL, "cost" numeric(3,1) NOT NULL, "is_credited" boolean NOT NULL DEFAULT false, "type" "public"."webhook_notifications_type_enum" NOT NULL, "webhook_url" character varying, "message_payload" jsonb, "created_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_ae644eb6677f6e4ac17f6765399" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "refresh_token"`);
         await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "credits_used"`);
         await queryRunner.query(`ALTER TABLE "twitch_streamer" ADD "is_partner" boolean NOT NULL DEFAULT false`);
-        await queryRunner.query(`ALTER TABLE "users" ADD "balance" numeric(3,1) NOT NULL`);
-        await queryRunner.query(`CREATE TYPE "public"."users_status_enum" AS ENUM('active', 'blocked')`);
-        await queryRunner.query(`ALTER TABLE "users" ADD "status" "public"."users_status_enum" NOT NULL DEFAULT 'active'`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "balance" numeric(3,1) NOT NULL DEFAULT '0'`);
+        await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "balance" DROP DEFAULT`);
         await queryRunner.query(`ALTER TABLE "users" ADD "discord_token_expires_at" date`);
         await queryRunner.query(`ALTER TABLE "users" ADD "twitch_access_token" character varying`);
         await queryRunner.query(`ALTER TABLE "users" ADD "twitch_refresh_token" character varying`);
         await queryRunner.query(`ALTER TABLE "users" ADD "twitch_token_expires_at" date`);
         await queryRunner.query(`ALTER TABLE "twitch_streamer_events" ADD "app_cost" numeric(3,1) NOT NULL DEFAULT '1'`);
-        await queryRunner.query(`ALTER TYPE "public"."roles_permissions_enum" RENAME TO "roles_permissions_enum_old"`);
-        await queryRunner.query(`CREATE TYPE "public"."roles_permissions_enum" AS ENUM('admin', 'read_apps', 'manage_apps', 'read_roles', 'manage_roles')`);
-        await queryRunner.query(`ALTER TABLE "roles" ALTER COLUMN "permissions" TYPE "public"."roles_permissions_enum"[] USING "permissions"::"text"::"public"."roles_permissions_enum"[]`);
-        await queryRunner.query(`DROP TYPE "public"."roles_permissions_enum_old"`);
         await queryRunner.query(`ALTER TABLE "twitch_apps" ALTER COLUMN "max_cost" SET DEFAULT '9500'`);
-        await queryRunner.query(`ALTER TABLE "user_sessions" ADD CONSTRAINT "FK_e9658e959c490b0a634dfc54783" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "discord_notifications" ADD CONSTRAINT "FK_8c33ffefce3671373a0b4e7e27b" FOREIGN KEY ("owner_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "discord_notifications" ADD CONSTRAINT "FK_639f7ddd705e903f45747ff1596" FOREIGN KEY ("guild_id") REFERENCES "discord_guilds"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "discord_notifications" ADD CONSTRAINT "FK_197b793441956ff312674aec7f0" FOREIGN KEY ("streamer_event_id") REFERENCES "twitch_streamer_events"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
@@ -41,30 +33,21 @@ export class GlobalDbChanges1780093794024 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "discord_notifications" DROP CONSTRAINT "FK_197b793441956ff312674aec7f0"`);
         await queryRunner.query(`ALTER TABLE "discord_notifications" DROP CONSTRAINT "FK_639f7ddd705e903f45747ff1596"`);
         await queryRunner.query(`ALTER TABLE "discord_notifications" DROP CONSTRAINT "FK_8c33ffefce3671373a0b4e7e27b"`);
-        await queryRunner.query(`ALTER TABLE "user_sessions" DROP CONSTRAINT "FK_e9658e959c490b0a634dfc54783"`);
         await queryRunner.query(`ALTER TABLE "twitch_apps" ALTER COLUMN "max_cost" SET DEFAULT '9000'`);
-        await queryRunner.query(`CREATE TYPE "public"."roles_permissions_enum_old" AS ENUM('admin', 'read_apps', 'manage_apps')`);
-        await queryRunner.query(`ALTER TABLE "roles" ALTER COLUMN "permissions" TYPE "public"."roles_permissions_enum_old"[] USING "permissions"::"text"::"public"."roles_permissions_enum_old"[]`);
-        await queryRunner.query(`DROP TYPE "public"."roles_permissions_enum"`);
-        await queryRunner.query(`ALTER TYPE "public"."roles_permissions_enum_old" RENAME TO "roles_permissions_enum"`);
         await queryRunner.query(`ALTER TABLE "twitch_streamer_events" DROP COLUMN "app_cost"`);
         await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "twitch_token_expires_at"`);
         await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "twitch_refresh_token"`);
         await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "twitch_access_token"`);
         await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "discord_token_expires_at"`);
-        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "status"`);
-        await queryRunner.query(`DROP TYPE "public"."users_status_enum"`);
         await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "balance"`);
         await queryRunner.query(`ALTER TABLE "twitch_streamer" DROP COLUMN "is_partner"`);
         await queryRunner.query(`ALTER TABLE "users" ADD "credits_used" integer NOT NULL DEFAULT '0'`);
-        await queryRunner.query(`ALTER TABLE "users" ADD "refresh_token" character varying`);
         await queryRunner.query(`DROP TABLE "webhook_notifications"`);
         await queryRunner.query(`DROP TYPE "public"."webhook_notifications_type_enum"`);
         await queryRunner.query(`DROP TABLE "discord_notifications"`);
         await queryRunner.query(`DROP TYPE "public"."discord_notifications_cost_type_enum"`);
         await queryRunner.query(`DROP TYPE "public"."discord_notifications_status_enum"`);
         await queryRunner.query(`DROP TABLE "discord_guilds"`);
-        await queryRunner.query(`DROP TABLE "user_sessions"`);
     }
 
 }
